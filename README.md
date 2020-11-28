31ac2ea9304a550224ebda651fe10436770a6b86
Stefan N <stefnnn@users.noreply.github.com>
2020-11-28 21:19:30 +0100

    Add ability to specify import table

--- README.md
+++ README.md
@@ -1,17 +1,21 @@
 # swissvotes-importer
+
 A tool to import all historic and actual votes in Switzerland from swissvotes.ch into a postgres database
 
 ## About
+
 swissvotes.ch contains a database with all present and historic votes (initiatives, referendums) that where ever held in Switzerland since 1848. This tool will help you download the latest version from the site and import the data into a postgres database.
 
 ## Run
+
 You can the tool by passing the connection arguments for you postgres database as needed
 
 ```bash
-python importvotes.py -u <DBUSER> -p <DBPASS> -h <DBHOST> -d <DBNAME>
+python importvotes.py -u <DBUSER> -p <DBPASS> -h <DBHOST> -d <DBNAME> -t <DBTABLE>
 ```
 
 which will generate:
+
 ```
 1) Connecting to database swissvotes
 2) Downloading and parsing latest swissvotes_dataset.csv
@@ -20,8 +24,9 @@ which will generate:
 ```
 
 ## Documentation
+
 The data format is documented in a (german) cookbook [here on swissvotes.ch](https://swissvotes.ch/page/dataset/codebook-de.pdf)
 
 ## Copyright
-This script is provided under [AGPL-3.0](./LICENSE). The data is provided freely but the source swissvotes.ch needs to be mentioned, see [here](https://swissvotes.ch/page/dataset)
 
+This script is provided under [AGPL-3.0](./LICENSE). The data is provided freely but the source swissvotes.ch needs to be mentioned, see [here](https://swissvotes.ch/page/dataset)
--- importvotes.py
+++ importvotes.py
@@ -1,4 +1,4 @@
-#!/usr/local/bin/python
+#!/usr/bin/env python3
 #
 # This scripts imports the lhe latest dataset from swissvotes.ch into a postgres db
 #
@@ -16,13 +16,14 @@ DOWNLOAD_URL = "https://swissvotes.ch/page/dataset/swissvotes_dataset.csv"
 try:
     # Define the getopt parameters
     opts, args = getopt.getopt(
-        sys.argv[1:], 'u:p:h:d:')
+        sys.argv[1:], 'u:p:h:d:t:')
 
     values = dict(opts)
     user = values.get("-u", "")
     password = values.get("-p", "")
     host = values.get("-p", "localhost")
-    database = values.get("-p", "swissvotes")
+    database = values.get("-d", "swissvotes")
+    table = values.get("-t", "swissvotes")
     userpass = user + (":%s" % password if password else "")
     if (userpass):
         userpass = userpass + "@"
@@ -30,7 +31,7 @@ try:
     PG_URL = "postgresql://%s%s:5432/%s" % (userpass, host, database)
 
 except getopt.GetoptError:
-    print('Usage: importvotes.py -u dbuser -p S3cure -h localhost -d swissvotes ')
+    print('Usage: importvotes.py -u dbuser -p S3cure -h localhost -d mydb -t mytable')
     sys.exit(2)
 
 print("1) Connecting to database %s" % database)
@@ -45,11 +46,7 @@ except exc.OperationalError:
 
 print("2) Downloading and parsing latest swissvotes_dataset.csv")
 # Load in the data
-df = pd.read_csv(
-    DOWNLOAD_URL,
-    encoding='latin_1',
-    delimiter=';'
-)
+df = pd.read_csv(DOWNLOAD_URL, encoding='iso-8859-1', delimiter=';')
 
 df = df.dropna(how='all', axis=1)
 print("3) Parsed %d lines" % len(df))
@@ -57,9 +54,10 @@ print("3) Parsed %d lines" % len(df))
 # Save the data from dataframe to
 # postgres table "iris_dataset"
 df.to_sql(
-    'swissvotes',
+    table,
     engine,
     if_exists='replace',
     index=False
 )
-print("4) (Re-)created table and wrote data")
+print("4) (Re-)created table and wrote data to table %s/%s" %
+      (database, table))
